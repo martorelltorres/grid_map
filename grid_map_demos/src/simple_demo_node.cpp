9875ed2d0ea659673c1bdec41f870e5bc21e6dee
antoni_martorell <antonimartorelltorres@gmail.com>
2022-01-10 13:48:47 +0100

    grid_map definition

--- grid_map_demos/src/simple_demo_node.cpp
+++ grid_map_demos/src/simple_demo_node.cpp
@@ -2,20 +2,34 @@
 #include <grid_map_ros/grid_map_ros.hpp>
 #include <grid_map_msgs/GridMap.h>
 #include <cmath>
+#include <iostream>
+#include <math.h>
 
 using namespace grid_map;
 
 int main(int argc, char** argv)
 {
+  // Node parameters
+    int length; 
+    int size; 
+    int resolution;
+
   // Initialize node and publisher.
   ros::init(argc, argv, "grid_map_simple_demo");
   ros::NodeHandle nh("~");
+
+    nh.param("length", length, 50);
+    nh.param("size", size, 50);
+    nh.param("resolution", resolution, 1);
   ros::Publisher publisher = nh.advertise<grid_map_msgs::GridMap>("grid_map", 1, true);
+
+
+
 
   // Create grid map.
   GridMap map({"elevation"});
-  map.setFrameId("map");
-  map.setGeometry(Length(1.2, 2.0), 0.03);
+  map.setFrameId("world_ned");
+  map.setGeometry(Length(size, length), resolution);
   ROS_INFO("Created map with size %f x %f m (%i x %i cells).",
     map.getLength().x(), map.getLength().y(),
     map.getSize()(0), map.getSize()(1));
@@ -29,7 +43,7 @@ int main(int argc, char** argv)
     for (GridMapIterator it(map); !it.isPastEnd(); ++it) {
       Position position;
       map.getPosition(*it, position);
-      map.at("elevation", *it) = -0.04 + 0.2 * std::sin(3.0 * time.toSec() + 5.0 * position.y()) * position.x();
+      map.at("elevation", *it) =   5 + std::sin(position.x()/7);
     }
 
     // Publish grid map.
